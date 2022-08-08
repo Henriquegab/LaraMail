e509dbd2b835081613314ba546a418d17ed5f18f
Henrique Gabriel Siqueira da Cruz <henrique.cruz@campus.unimontes.int>
2022-08-08 16:09:25 -0300

    Logic to send an email

---
+++ resources/views/emails/sendMail.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+# Introduction
+
+O corpo da mensagem!
+
+{{-- <h1>{{ $details['title'] }}</h1>
+<p>{{ $details['body'] }}</p> --}}
+
+@component('mail::button', ['url' => ''])
+Botão
+@endcomponent
+
+VLW,<br>
+{{ config('app.name') }}
+@endcomponent
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\LaraMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('send-mail', function () {
+
+    $details = [
+        'title' => 'Mail from ItSolutionStuff.com',
+        'body' => 'This is for testing email using smtp'
+    ];
+
+    Mail::to('henriquepro8@gmail.com')->send(new LaraMail($details));
+
+    dd("Email is Sent.");
 });
